7ec19b895cee549a97d76281fc4625fde1a507cf
Reed Miller <nile@techimpact.org>
2023-03-19 15:00:20 -0400

    add latest data

--- etl.R
+++ etl.R
@@ -1,13 +1,14 @@
 pacman::p_load("dplyr", "readr", "readxl", "jsonlite", "here")
 
-acled_data <- readxl::read_xlsx(here::here("data/Ukraine_Black_Sea_2020_2023_Mar10.xlsx"))
+acled_data <- readxl::read_xlsx(here::here("data/latest.xlsx"))
 
 ua <- acled_data %>%
   filter(COUNTRY == "Ukraine" & as.Date(EVENT_DATE) >= as.Date("2022-02-23")) %>% select(EVENT_DATE, YEAR, EVENT_ID_CNTY, EVENT_TYPE, SUB_EVENT_TYPE, FATALITIES, ACTOR1, ASSOC_ACTOR_1,ACTOR2,ASSOC_ACTOR_2, ADMIN1, ADMIN2, ADMIN3, LOCATION, LATITUDE, LONGITUDE, SOURCE, NOTES, TIMESTAMP)
 
 ua_summed <- ua %>%
   group_by(EVENT_DATE, ADMIN1, EVENT_TYPE) %>%
-  summarize(incidents = n(), fatalities = sum(FATALITIES, na.rm=T))
+  summarize(incidents = n(), fatalities = sum(FATALITIES, na.rm=T)) %>%
+  filter(!is.na(ADMIN1))
 
 jsonlite::minify(jsonlite::toJSON(ua_summed,auto_unbox = T)) %>%
-  readr::write_file(here("ua_3-10-23-summed.json"),append = F)
+  readr::write_file(here("ua_latest-summed.json"),append = F)
