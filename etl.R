28a4a6172090130ba8feac9fa211de7a3bdcfb79
Reed Miller <nile@techimpact.org>
2023-03-20 12:45:03 -0400

    change start date to beginning of 2022

--- etl.R
+++ etl.R
@@ -3,7 +3,7 @@ pacman::p_load("dplyr", "readr", "readxl", "jsonlite", "here")
 acled_data <- readxl::read_xlsx(here::here("data/latest.xlsx"))
 
 ua <- acled_data %>%
-  filter(COUNTRY == "Ukraine" & as.Date(EVENT_DATE) >= as.Date("2022-02-23")) %>% select(EVENT_DATE, YEAR, EVENT_ID_CNTY, EVENT_TYPE, SUB_EVENT_TYPE, FATALITIES, ACTOR1, ASSOC_ACTOR_1,ACTOR2,ASSOC_ACTOR_2, ADMIN1, ADMIN2, ADMIN3, LOCATION, LATITUDE, LONGITUDE, SOURCE, NOTES, TIMESTAMP)
+  filter(COUNTRY == "Ukraine" & as.Date(EVENT_DATE) >= as.Date("2022-01-01")) %>% select(EVENT_DATE, YEAR, EVENT_ID_CNTY, EVENT_TYPE, SUB_EVENT_TYPE, FATALITIES, ACTOR1, ASSOC_ACTOR_1,ACTOR2,ASSOC_ACTOR_2, ADMIN1, ADMIN2, ADMIN3, LOCATION, LATITUDE, LONGITUDE, SOURCE, NOTES, TIMESTAMP)
 
 ua_summed <- ua %>%
   group_by(EVENT_DATE, ADMIN1, EVENT_TYPE) %>%
